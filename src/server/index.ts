3b19aa7a35ceed8383f8155181c36048f2256221
rekozzz <rekoshehata@gmail.com>
2026-09-21 17:51:35 +0300

    decoupling

--- src/server/index.ts
+++ src/server/index.ts
@@ -2,6 +2,7 @@ import amqp from "amqplib";
 import { publishJSON } from "../internal/pubsub/publish.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
 import type { PlayingState } from "../internal/gamelogic/gamestate.js";
+import { getInput, printServerHelp } from "../internal/gamelogic/gamelogic.js";
 
 async function main() {
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
@@ -12,11 +13,38 @@ async function main() {
 
   console.log("Connected to RabbitMQ");
 
- const state: PlayingState = {
-    isPaused : true,
- }
+  printServerHelp();
 
-  await publishJSON(ch, ExchangePerilDirect, PauseKey, state)
+  while (true) {
+    const words = await getInput();
+
+    if (words.length === 0) {
+      continue;
+    }
+
+    if (words[0] === "pause") {
+      console.log("Sending pause message");
+
+      const state: PlayingState = {
+        isPaused: true,
+      };
+
+      await publishJSON(ch, ExchangePerilDirect, PauseKey, state);
+    } else if (words[0] === "resume") {
+      console.log("Sending resume message");
+
+      const state: PlayingState = {
+        isPaused: false,
+      };
+
+      await publishJSON(ch, ExchangePerilDirect, PauseKey, state);
+    } else if (words[0] === "quit") {
+      console.log("Exiting...");
+      break;
+    } else {
+      console.log("I don't understand this message");
+    }
+  }
 
   process.on("SIGINT", async () => {
     console.log("Shutting down...");
