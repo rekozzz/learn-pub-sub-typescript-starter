a1d97acde25eb04fe347c74bbfc658c050c5a030
rekozzz <rekoshehata@gmail.com>
2026-09-21 15:11:46 +0300

    first exchange

---
+++ src/internal/pubsub/publish.ts
@@ -0,0 +1,25 @@
+import type { ConfirmChannel } from "amqplib";
+
+export function publishJSON<T>(
+  ch: ConfirmChannel,
+  exchange: string,
+  routingKey: string,
+  value: T,
+): Promise<void> {
+  const content = Buffer.from(JSON.stringify(value));
+
+  return new Promise((resolve, reject) => {
+    ch.publish(exchange, routingKey, content,
+      {
+        contentType: "application/json",
+      },
+      (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      },
+    );
+  });
+}
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,11 +1,22 @@
 import amqp from "amqplib";
+import { publishJSON } from "../internal/pubsub/publish.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import type { PlayingState } from "../internal/gamelogic/gamestate.js";
 
 async function main() {
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
 
   const conn = await amqp.connect(rabbitConnString);
 
+  const ch = await conn.createConfirmChannel();
+
   console.log("Connected to RabbitMQ");
+
+ const state: PlayingState = {
+    isPaused : true,
+ }
+
+  await publishJSON(ch, ExchangePerilDirect, PauseKey, state)
 
   process.on("SIGINT", async () => {
     console.log("Shutting down...");
