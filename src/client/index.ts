d45f7b249da270f0feac38749ca526c9d5c2711c
rekozzz <rekoshehata@gmail.com>
2026-09-21 18:15:20 +0300

    REPL for client

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,7 +1,10 @@
 import amqp from "amqplib";
-import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import { clientWelcome, commandStatus, getInput, printClientHelp, printQuit } from "../internal/gamelogic/gamelogic.js";
 import { declareAndBind, SimpleQueueType } from "../internal/pubsub/consume.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { GameState } from "../internal/gamelogic/gamestate.js";
+import { commandSpawn } from "../internal/gamelogic/spawn.js";
+import { commandMove } from "../internal/gamelogic/move.js";
 
 async function main() {
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
@@ -12,6 +15,49 @@ async function main() {
     const username = await clientWelcome();
   
     const [ch, queue] = await declareAndBind(conn, ExchangePerilDirect, `pause.${username}`, PauseKey, SimpleQueueType.Transient)
+
+    const gameState = new GameState(username);
+
+    while (true) {
+  const words = await getInput();
+
+  if (words.length === 0) {
+    continue;
+  }
+
+  const command = words[0];
+
+  if (command === "spawn") {
+    try {
+      const unit = commandSpawn(gameState, words);
+      console.log(unit);
+    } catch (err) {
+      if (err instanceof Error) {
+        console.log(err.message);
+      }
+    }
+  } else if (command === "move") {
+    try {
+      commandMove(gameState, words);
+      console.log("Unit moved successfully");
+    } catch (err) {
+      if (err instanceof Error) {
+        console.log(err.message);
+      }
+    }
+  } else if (command === "status") {
+    commandStatus(gameState);
+  } else if (command === "help") {
+    printClientHelp();
+  } else if (command === "spam") {
+    console.log("Spamming not allowed yet!");
+  } else if (command === "quit") {
+    printQuit();
+    break;
+  } else {
+    console.log("Unknown command");
+  }
+}
 }
 
 main().catch((err) => {
