85486fb74fd6bb3c2d5808f4a78d9c716042f13f
rekozzz <rekoshehata@gmail.com>
2026-09-21 03:17:27 +0300

    intial

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,8 +1,18 @@
+import amqp from "amqplib";
+
 async function main() {
-  console.log("Starting Peril server...");
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
+
+  const conn = await amqp.connect(rabbitConnString);
+
+  console.log("Connected to RabbitMQ");
+
+  process.on("SIGINT", async () => {
+    console.log("Shutting down...");
+
+    await conn.close();
+    process.exit(0);
+  });
 }
 
-main().catch((err) => {
-  console.error("Fatal error:", err);
-  process.exit(1);
-});
+main();
